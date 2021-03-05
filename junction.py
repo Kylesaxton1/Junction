64b0f2601a7f04c371237eb3d6cd0f62a2f02465
Kylesaxton1 <79671903+Kylesaxton1@users.noreply.github.com>
2021-03-04 20:44:33 -0500

    Update junction.py

--- junction.py
+++ junction.py
@@ -3,6 +3,12 @@ import time
 userName = input("What is your name?")
 print("This is a Junction Program, you will input two strings and we will return to you the shared characters, or junction between them")
 
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+NUMBERS = "1234567890"
+VOWELS = "AEIOUaeiou"
+
+METRICS = [LETTERS,VOWELS,NUMBERS]
+
 time.sleep(1)
 inputOne = input("What is the first string that you want to enter?")
 inputTwo = input("What is the secod=nd string that you want to enter?")
@@ -14,3 +20,17 @@ for c in inputOne:
     junct += c
     
 print(junct)
+vowels = 0
+letters = 0
+numbers = 0
+
+for i in junct:
+  if i in LETTERS:
+    letters += 1
+    if i in VOWELS:
+      vowels += 1
+   if i in NUMBERS:
+    numbers += 1
+    
+print("of the characters that the two share,", vowels, "of them are vowels")
+print("of the characters that the two share,", numbers, "of them are numbers")
