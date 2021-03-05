a4c0696d30c15b5dcbc64e5d79c14500186558b3
Kylesaxton1 <79671903+Kylesaxton1@users.noreply.github.com>
2021-03-04 20:26:33 -0500

    Update junction.py

--- junction.py
+++ junction.py
@@ -1,1 +1,16 @@
+import time
+
 userName = input("What is your name?")
+print("This is a Junction Program, you will input two strings and we will return to you the shared characters, or junction between them")
+
+time.sleep(1)
+inputOne = input("What is the first string that you want to enter?")
+inputTwo = input("What is the secod=nd string that you want to enter?")
+
+junct = ''
+
+for c in inputOne:
+  if c in inputTwo and c not in junct:
+    junct += c
+    
+print(junct)
